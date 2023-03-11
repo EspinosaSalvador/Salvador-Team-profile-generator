ffe055d1d0464d0ff573ba0bf7519a7d8c3e0a52
Salvador Espinosa <espinosalvador94@outlook.com>
2023-03-10 18:06:17 -0600

    added code to Employee.test due that I am going to start running test on the code I created on Employee js on the lib

---
+++ test/Employee.test.js
@@ -0,0 +1,4 @@
+const Employee = require("../lib/Employee");
+// ! to see if the project is working so far I am going to run test on it.
+// * I am importing the require of Employee from lib.js employee due that we are seeing if what I did so far works on the system.
+const Employee = require("../lib/Employee");
