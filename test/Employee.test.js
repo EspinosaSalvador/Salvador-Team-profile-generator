60525733cff30e3383ff67914a42a62edb9c5f6c
Salvador Espinosa <espinosalvador94@outlook.com>
2023-03-10 18:08:00 -0600

    eliminated extra require that was repeted

--- test/Employee.test.js
+++ test/Employee.test.js
@@ -1,4 +1,3 @@
-const Employee = require("../lib/Employee");
 // ! to see if the project is working so far I am going to run test on it.
 // * I am importing the require of Employee from lib.js employee due that we are seeing if what I did so far works on the system.
 const Employee = require("../lib/Employee");
